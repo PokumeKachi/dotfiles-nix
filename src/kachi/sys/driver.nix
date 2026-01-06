e79b71f2eb499cbf79bc97e3cc5d9aaaf64b2855
Kachi <108186213+PokumeKachi@users.noreply.github.com>
2026-01-06 16:09:18 +0700

    feat: added some drivers

--- src/kachi/sys/driver.nix
+++ src/kachi/sys/driver.nix
@@ -5,54 +5,69 @@
         "modesetting"
     ];
 
+    boot.kernelParams = [ "i915.force_probe=46d1,i915.enable_guc=3" ];
+
     boot.kernelModules = [ "i915" ];
 
-    hardware.graphics = {
-        enable = true;
-        enable32Bit = true;
-        extraPackages = with pkgs; [
-            libglvnd
-            intel-compute-runtime
-            libvdpau-va-gl
-            vpl-gpu-rt
+    hardware = {
+        enableAllFirmware = true;
+        intel-gpu-tools.enable = true;
+        graphics = {
+            enable = true;
+            enable32Bit = true;
+            extraPackages = with pkgs; [
+                libglvnd
+                intel-compute-runtime
+                libvdpau-va-gl
 
-            vaapi-intel-hybrid
-            intel-media-driver
+                vpl-gpu-rt
+                vaapi-intel-hybrid
+                libva-vdpau-driver
+                intel-media-driver
+                intel-vaapi-driver
 
-            libva
-            libva-utils
-            libva-vdpau-driver
+                libva
+                libva-utils
 
-            mesa
-            mesa-demos
-            vulkan-loader
+                x264
+                x265
+                openh264
 
-            cairo
-            pango
+                mesa
+                mesa-demos
+                vulkan-loader
 
-            libsForQt5.qt5.qttools
-            libsForQt5.qt5.qtbase
-            libsForQt5.qt5.qtwayland
-            kdePackages.qtwayland
+                cairo
+                pango
 
-            glib
-            gdk-pixbuf
+                libsForQt5.qt5.qttools
+                libsForQt5.qt5.qtbase
+                libsForQt5.qt5.qtwayland
+                kdePackages.qtwayland
 
-            xorg.libxcb
-            xorg.xcbutil
-            xorg.libX11
-            xorg.libXcursor
-            xorg.libXrandr
-            xorg.libXfixes
-            xorg.libXi
-            xorg.libXinerama
-            xorg.libXext
-            xorg.libXxf86vm
+                glib
+                gdk-pixbuf
 
-            libGL
-            glfw
-            libglvnd
+                xorg.libxcb
+                xorg.xcbutil
+                xorg.libX11
+                xorg.libXcursor
+                xorg.libXrandr
+                xorg.libXfixes
+                xorg.libXi
+                xorg.libXinerama
+                xorg.libXext
+                xorg.libXxf86vm
 
-        ];
+                libGL
+                glfw
+                libglvnd
+
+            ];
+        };
     };
+
+    environment.sessionVariables.LIBVA_DRIVER_NAME = "iHD";
+
+    hardware.cpu.intel.updateMicrocode = true;
 }

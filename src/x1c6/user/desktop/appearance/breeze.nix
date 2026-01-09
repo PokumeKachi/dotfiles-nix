2da599ba24969a43b2bf8d67f60ce0f40664615f
Kachi <108186213+PokumeKachi@users.noreply.github.com>
2026-01-09 16:36:16 +0700

    feat: full migration complete

--- src/x1c6/user/desktop/appearance/breeze.nix
+++ src/x1c6/user/desktop/appearance/breeze.nix
@@ -1,49 +1,55 @@
 { pkgs, ... }:
 {
-#     home.pointerCursor =
-#         {
-#                 gtk.enable = true;
-#                 x11.enable = true;
-#                 name = "capitaine-cursors";
-#                 size = 48;
-#                 package = pkgs.capitaine-cursors;
-#             };
-#
-#     gtk = {
-#         enable = true;
-#         colorScheme = "dark";
-#         iconTheme = {
-#             name = "oomox-gruvbox-dark";
-#             package = pkgs.gruvbox-dark-icons-gtk;
-#         };
-#         cursorTheme = {
-#             name = "capitaine-cursors";
-#             package = pkgs.capitaine-cursors;
-#         };
-#         # theme = {
-#         #     name = "Awesthetic-dark";
-#         # };
-#         theme = {
-#             name = "breeze-dark";
-#             package = pkgs.kdePackages.breeze-gtk;
-#         };
-#         # iconTheme = {
-#         #     name = "breeze";
-#         #     package = pkgs.kdePackages.breeze-icons;
-#         # };
-#         #
-#         # cursorTheme = {
-#         #     name = "breeze";
-#         #     package = pkgs.kdePackages.breeze-gtk;
-#         # };
-#     };
-#
-#     qt = {
-#         enable = true;
-#         platformTheme.name = "kde";
-#         style = {
-#             name = "breeze-dark";
-#             package = pkgs.kdePackages.breeze;
-#         };
-#     };
+    environment.systemPackages = with pkgs; [
+        kdePackages.breeze
+        kdePackages.breeze-gtk
+        kdePackages.breeze-icons
+
+    ];
+    #     home.pointerCursor =
+    #         {
+    #                 gtk.enable = true;
+    #                 x11.enable = true;
+    #                 name = "capitaine-cursors";
+    #                 size = 48;
+    #                 package = pkgs.capitaine-cursors;
+    #             };
+    #
+    #     gtk = {
+    #         enable = true;
+    #         colorScheme = "dark";
+    #         iconTheme = {
+    #             name = "oomox-gruvbox-dark";
+    #             package = pkgs.gruvbox-dark-icons-gtk;
+    #         };
+    #         cursorTheme = {
+    #             name = "capitaine-cursors";
+    #             package = pkgs.capitaine-cursors;
+    #         };
+    #         # theme = {
+    #         #     name = "Awesthetic-dark";
+    #         # };
+    #         theme = {
+    #             name = "breeze-dark";
+    #             package = pkgs.kdePackages.breeze-gtk;
+    #         };
+    #         # iconTheme = {
+    #         #     name = "breeze";
+    #         #     package = pkgs.kdePackages.breeze-icons;
+    #         # };
+    #         #
+    #         # cursorTheme = {
+    #         #     name = "breeze";
+    #         #     package = pkgs.kdePackages.breeze-gtk;
+    #         # };
+    #     };
+    #
+    #     qt = {
+    #         enable = true;
+    #         platformTheme.name = "kde";
+    #         style = {
+    #             name = "breeze-dark";
+    #             package = pkgs.kdePackages.breeze;
+    #         };
+    #     };
 }

5d7edeae499b99ead19403534f4571001d38bea4
Kachi <108186213+PokumeKachi@users.noreply.github.com>
2026-01-13 09:02:15 +0700

    feat: shi

--- src/x1c6/user/terminal/tui/neomutt.nix
+++ src/x1c6/user/terminal/tui/neomutt.nix
@@ -1,11 +1,11 @@
 {
   # programs.neomutt = {
   #   enable = true;
-  #   extraConfig = ''
-  #     set folder = "~/media/mail"            # base mail folder
-  #     set record = "+Sent"             # sent mail folder inside folder
-  #     set postponed = "+Drafts"        # drafts folder
-  #     set spoolfile = "+INBOX"         # inbox folder
-  #   '';
+  #   # extraConfig = ''
+  #   #   set folder = "~/media/mail"            # base mail folder
+  #   #   set record = "+Sent"             # sent mail folder inside folder
+  #   #   set postponed = "+Drafts"        # drafts folder
+  #   #   set spoolfile = "+INBOX"         # inbox folder
+  #   # '';
   # };
 }

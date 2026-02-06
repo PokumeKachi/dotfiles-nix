79a6406c2544dae957b6ee11440a10a83caadc3d
Kachi <108186213+PokumeKachi@users.noreply.github.com>
2026-02-06 16:33:01 +0700

    feat: power tweaks

--- src/x1c6/os/extra/optimization/powersave.nix
+++ src/x1c6/os/extra/optimization/powersave.nix
@@ -28,7 +28,7 @@
                 charger = {
                     governor = "powersave";
                     # energy_performance_preference = 160;
-                    energy_performance_preference = 0;
+                    energy_performance_preference = 150;
                     energy_perf_bias = 15;
                     # cpu becomes inefficient past 2.4 ghz
                     # scaling_min_freq =  400000;
@@ -40,7 +40,7 @@
                 battery = {
                     governor = "powersave";
                     # energy_performance_preference = 160;
-                    energy_performance_preference = 0;
+                    energy_performance_preference = 150;
                     energy_perf_bias = 15;
                     # cpu becomes inefficient past 2.4 ghz
                     # scaling_min_freq =  400000;
@@ -77,8 +77,8 @@
                 SOUND_POWER_SAVE_ON_AC = 0;
                 SOUND_POWER_SAVE_ON_BAT = 1;
 
-                START_CHARGE_THRESH_BAT0 = 55;
-                STOP_CHARGE_THRESH_BAT0 = 60;
+                START_CHARGE_THRESH_BAT0 = 75;
+                STOP_CHARGE_THRESH_BAT0 = 80;
 
                 RESTORE_DEVICE_STATE_ON_STARTUP = 1;
                 RESTORE_THRESHOLDS_ON_BAT = 1;

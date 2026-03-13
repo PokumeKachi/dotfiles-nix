32dc62ce7032ba407599756c981387ef7411116d
Kachi <108186213+PokumeKachi@users.noreply.github.com>
2026-03-13 07:56:22 +0700

    feat: add stuff

--- src/x1c6/user/desktop/services/printing.nix
+++ src/x1c6/user/desktop/services/printing.nix
@@ -2,18 +2,40 @@
 {
     services.printing = {
         enable = true;
-        listenAddresses = [ "*:631" ];
-        allowFrom = [ "all" ];
-        browsing = true;
-        defaultShared = true;
         openFirewall = true;
+        browsing = true;
+        drivers = with pkgs; [
+            canon-cups-ufr2
+            canon-capt
+            (pkgs.stdenv.mkDerivation {
+                pname = "canon6030-driver";
+                version = "1";
+
+                src = ./canon6030;
+                dontBuild = true;
+
+                nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+                buildInputs = [ pkgs.cups ];
+
+                installPhase = ''
+                    mkdir -p $out/share/cups/model
+                    mkdir -p $out/lib/cups/filter
+
+                    cp CNRCUPSLBP6030ZNK.ppd $out/share/cups/model/
+                    install -Dm755 rastertosfp $out/lib/cups/filter/rastertosfp
+                '';
+            })
+        ];
     };
-    services.printing.drivers = with pkgs; [
-        canon-cups-ufr2
-    ];
+
     services.avahi = {
         enable = true;
         nssmdns4 = true;
         openFirewall = true;
     };
+
+    users.users.kachi.extraGroups = [
+        "lp"
+        "lpadmin"
+    ];
 }

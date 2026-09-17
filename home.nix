09ab31e0040ccb72465e5adf7113cc189016a2aa
PokumeKachi <108186213+PokumeKachi@users.noreply.github.com>
2026-09-17 11:01:54 +0700

    feat: add niri

--- home.nix
+++ home.nix
@@ -1,11 +1,20 @@
-{ config, pkgs, ... }:
 {
+    inputs,
+    config,
+    pkgs,
+    ...
+}:
+{
+    imports = [
+        inputs.noctalia.homeModules.default
+    ];
+
     home.username = "kachi";
     home.homeDirectory = "/home/kachi";
-
     home.stateVersion = "26.05";
 
     home.packages = with pkgs; [
+        # CLI / development
         koreader
         direnv
         just
@@ -21,6 +30,35 @@
         btop
         gitui
         flatpak
+
+        # Niri utilities / apps referenced by config.kdl
+        kitty
+        tofi
+        swaylock
+        wdisplays
+        brightnessctl
+        wlogout
+        wofi-emoji
+
+        # Session / Wayland
+        xwayland-satellite
+
+        # Wallpaper / notifications
+        hyprpaper
+        mako
+
+        # XDG Desktop Portals
+        xdg-desktop-portal
+        xdg-desktop-portal-gtk
+        xdg-desktop-portal-gnome
+        gnome-keyring
+
+        # Other startup applications
+        korganizer
+        keepassxc
+
+        # Input method
+        ibus
     ];
 
     home.pointerCursor = {
@@ -41,5 +79,15 @@
         VISUAL = "nvim";
     };
 
-    home.file = { };
+    programs.niri = {
+        enable = true;
+
+        # Keep your existing KDL config.
+        # Adjust this path to wherever you put it in your flake.
+        settings = {
+            # Put generated/simple settings here if desired.
+        };
+    };
+
+    programs.noctalia.enable = true;
 }
